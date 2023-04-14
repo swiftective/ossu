9a36e7c27248c5bd9d5a9a52997cc3337386aee1
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-04-14 18:18:14 +0530

    H/W

---
+++ core_programming/How_to_code_simple_data/interval_speeding.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(require test-engine/racket-tests)
+
+;; CarSpeed is Number[0, 200]
+;; Interp. the speed of a car in km/h: 0 means stopped, 200 is the maximum speed
+;; (define CS 0)
+;; (define CS2 200)
+;; (define CS3 100)
+
+#;
+(define (fn-for-carspeed cs)
+  (... cs)
+)
+
+;; Template rules used
+;; - Atomic non-distinct: Number[0, 200]
+
+;; CarSpeed -> Boolean
+;; produces true if car's speed is over speed limit, else false
+
+(check-expect (speeding? 0) #f) ; Tests
+(check-expect (speeding? 111) #t)
+
+;; (define (speeding? cs) true) ; Stub
+
+;; <use template from CarSpeed>
+
+(define (speeding? cs) (> cs 110))
+
+(test)
